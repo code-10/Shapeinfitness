28fb2c5de9d5467866dd76d4b9407feabacf608a
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-24 01:11:05 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 <footer class="page-footer font-small" style="background-color:black;">
 
   <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+  <div class="footer-copyright text-center py-3" style="color:white">© 2020 Copyright:
     <a href="#" style="color:white">Shapeinfitness</a>
   </div>
   <!-- Copyright -->
