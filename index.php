27bce55347c976fe1b967970c20ca0cc0ad8bd85
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-24 01:55:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -117,6 +117,153 @@
 
 
 
+<!--Gallery-->
+<div class="m-4" id="gallery">
+	<h1 class="font-weight-light text-center text-lg-left mt-4 mb-0">Gallery</h1>
+	<hr class="mt-2 mb-5">
+	<div class="row text-center text-lg-left">
+		<? $c=1; for($i=0;$i<8;$i++) { ?>
+		<div class="col-lg-3 col-md-4 col-6">	<a href="#gallery" class="d-block mb-4 h-100">
+            <img id="myImg<?=$i?>" class="img-fluid thumb-nail" src="../assets/main.jpg" alt="">
+          </a>
+		</div>
+		<div id="myModal<?=$i?>" class="modal"> <span class="close">&times;</span>
+			<img class="modal-content" id="img<?=$i?>">
+			<div id="caption<?=$i?>"></div>
+		</div>
+		<? $c++; } ?>
+	</div>
+</div>
+
+	
+	
+	
+
+<script>
+	var i;
+
+for(i=0;i<64;i++){
+// Get the modal
+var modal = document.getElementById("myModal"+i);
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg"+i);
+var modalImg = document.getElementById("img"+i);
+var captionText = document.getElementById("caption"+i);
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[i];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+}
+</script>
+
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+
+#myImg {
+  border-radius: 5px;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+#myImg:hover {opacity: 0.7;}
+
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.9); /* Black w/ opacity */
+}
+
+/* Modal Content (image) */
+.modal-content {
+  margin: auto;
+  display: block;
+  width: 80%;
+  max-width: 700px;
+}
+
+/* Caption of Modal Image */
+#caption {
+  margin: auto;
+  display: block;
+  width: 80%;
+  max-width: 700px;
+  text-align: center;
+  color: #ccc;
+  padding: 10px 0;
+  height: 150px;
+}
+
+/* Add Animation */
+.modal-content, #caption {  
+  -webkit-animation-name: zoom;
+  -webkit-animation-duration: 0.6s;
+  animation-name: zoom;
+  animation-duration: 0.6s;
+}
+
+@-webkit-keyframes zoom {
+  from {-webkit-transform:scale(0)} 
+  to {-webkit-transform:scale(1)}
+}
+
+@keyframes zoom {
+  from {transform:scale(0)} 
+  to {transform:scale(1)}
+}
+
+/* The Close Button */
+.close {
+  position: absolute;
+  top: 15px;
+  right: 35px;
+  color: #f1f1f1;
+  font-size: 40px;
+  font-weight: bold;
+  transition: 0.3s;
+}
+
+.close:hover,
+.close:focus {
+  color: #bbb;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+/* 100% Image Width on Smaller Screens */
+@media only screen and (max-width: 700px){
+  .modal-content {
+    width: 100%;
+  }
+}
+	@media (min-width:961px)  { .modal-content { object-fit: cover;width: 100%;height: 400px; } /* tablet, landscape iPad, lo-res laptops ands desktops */ }
+	@media (min-width:1025px) { .modal-content { object-fit: cover;width: 100%;height: 400px; } /* big landscape tablets, laptops, and desktops */ }
+	@media (min-width:1281px) { .modal-content { object-fit: cover;width: 100%;height: 400px; } /* hi-res laptops and desktops */ }
+</style>
+
+
+
+
+
+
+
 <?php include_once 'footer.php'; ?>
 
 
