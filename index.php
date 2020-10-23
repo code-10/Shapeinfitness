3e1f64156037a8e711993ab1381c3a1780aa3d57
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-24 00:54:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 
 <nav class="navbar sticky-top navbar-expand-lg navbar-dark" style="background-color:#000000;">
-  <a class="navbar-brand" href="#" style="color:black;">Shapeinfitness</a>
+  <a class="navbar-brand" href="#">Shapeinfitness</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -42,8 +42,8 @@
 	@media (min-width:320px)  {  /* smartphones, iPhone, portrait 480x320 phones */ }
 	@media (min-width:481px)  {  /* portrait e-readers (Nook/Kindle), smaller tablets @ 600 or @ 640 wide. */ }
  	@media (min-width:641px)  {  /* portrait tablets, portrait iPad, landscape e-readers, landscape 800x480 or 854x480 phones */ }
-	@media (min-width:961px)  { .main{object-fit: cover;width: 100%;height: 600px;} /* tablet, landscape iPad, lo-res laptops ands desktops */ }
-	@media (min-width:1025px) {  .main{object-fit: cover;width: 100%;height: 600px;} /* big landscape tablets, laptops, and desktops */ }
-	@media (min-width:1281px) {   .main{object-fit: cover;width: 100%;height: 600px;}/* hi-res laptops and desktops */ }
+	@media (min-width:961px)  { .main{object-fit: cover;width: 100%;height: 698px;} /* tablet, landscape iPad, lo-res laptops ands desktops */ }
+	@media (min-width:1025px) {  .main{object-fit: cover;width: 100%;height: 698px;} /* big landscape tablets, laptops, and desktops */ }
+	@media (min-width:1281px) {   .main{object-fit: cover;width: 100%;height: 698px;}/* hi-res laptops and desktops */ }
 	
 </style>
