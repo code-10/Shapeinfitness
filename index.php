68acecd17ad694b1417c7a0a38dc6b0afc75ffe2
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-24 00:16:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php?>
+<?php
+  include_once 'header.php';
+?>
+
+
+<nav class="nav">
+  <a class="nav-link active navi" href="#">Home</a>
+  <a class="nav-link navi" href="#">Location</a>
+  <a class="nav-link navi" href="#">Services</a>
+  <a class="nav-link navi" href="#">Gallery</a>
+  <a class="nav-link navi" href="#">Contact Us</a>
+</nav>
+
+
+<style>
+  .navi{ color:black; };
+</style>
