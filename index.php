9fce08b1cca3d87181e7c10c7bf291e358342e12
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-24 23:08:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -161,8 +161,8 @@
 				<button type="submit" name="check" id="sent" class="btn btn-dark" style="background-color:black;">Send</button>
 			</form>
 		</div>
-		<p id="location" style="font-family: 'Roboto', sans-serif;color:black;font-size:40px;" class="text-center mt-5 mb-2">LOCATION</p>
 		<div class="col-md-6 mt-4 mb-4" id="location">
+			<p id="location" style="font-family: 'Roboto', sans-serif;color:black;font-size:40px;" class="text-center mt-5 mb-2">LOCATION</p>
 			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15548.576152625568!2d77.5934389!3d13.0264981!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xafffc1080f8f8457!2sSHAPE%20IN%20-%20Women&#39;s%20Fitness%20Gym%20and%20Group%20classes%20RT%20NAGAR!5e0!3m2!1sen!2sin!4v1603537909194!5m2!1sen!2sin" width="100%" height="507" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 		</div>
 	</div>
