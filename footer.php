b1475f274093f8fbe969440293c632c267c640a1
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-24 18:12:07 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,11 +1,60 @@
-<?php include_once 'header.php' ?>
+<?php include_once 'header.php'; ?>
 
 <!-- Footer -->
-<footer class="page-footer font-small" style="background-color:black;">
+<footer class="page-footer font-small blue pt-4">
+
+  <!-- Footer Links -->
+  <div class="container-fluid text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-6 mt-md-0 mt-3">
+
+        <!-- Content -->
+        <h5 class="text-uppercase">Shape in fitness</h5>
+        <p>Women's Gym</p>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-6 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">A</a>
+          </li>
+          <li>
+            <a href="#!">B</a>
+          </li>
+          <li>
+            <a href="#!">C</a>
+          </li>
+          <li>
+            <a href="#!">D</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
 
   <!-- Copyright -->
-  <div class="footer-copyright text-center py-3" style="color:white">© 2020 Copyright:
-    <a href="#" style="color:white">Shapeinfitness</a>
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
   </div>
   <!-- Copyright -->
 
