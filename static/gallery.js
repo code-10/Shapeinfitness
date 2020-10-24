7c63f442b1d8c69e01892717f89cb0f06f5b5c49
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-24 16:18:28 +0530

    Update gallery.js

--- static/gallery.js
+++ static/gallery.js
@@ -1,1 +1,24 @@
-..
+
+	var i;
+
+for(i=0;i<8;i++){
+// Get the modal
+var modal = document.getElementById("myModal"+i);
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg"+i);
+var modalImg = document.getElementById("img"+i);
+var captionText = document.getElementById("caption"+i);
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[i];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+}
