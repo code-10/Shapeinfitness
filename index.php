fda00a37077301b8bea4a779b722c3b808075d04
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-24 19:47:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -105,7 +105,24 @@
 
 <!--Gallery-->
 
-
+<div class="container" id="gallery">
+   <h1 class="font-weight-light text-center text-lg-left mt-4 mb-0">Gallery</h1>
+   <hr class="mt-2 mb-5">
+   <div class="row text-center text-lg-left">
+      <? $c=1; for($i=0;$i<64;$i++) { ?>
+      <div class="col-lg-3 col-md-4 col-6">
+         <a href="#gallery" class="d-block mb-4 h-100">
+         <img id="myImg<?=$i?>" class="img-fluid thumb-nail" src="../assets/<?=$c?>.jpg" alt="">
+         </a>
+      </div>
+      <div id="myModal<?=$i?>" class="modal">
+         <span class="close">&times;</span>
+         <img class="modal-content" id="img<?=$i?>">
+         <div id="caption<?=$i?>"></div>
+      </div>
+      <? $c++; } ?>
+   </div>
+</div>
 
 <!--Gallery end-->
 
