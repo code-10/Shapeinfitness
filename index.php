63f47025ddf231725dfb1486fb5cc8f8f6f96647
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-24 17:59:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -116,7 +116,7 @@
 
 <div class="container">
 	<div class="row mb-5 mt-5">
-		<div class="col-md-6">
+		<div class="col-md-6 mt-4 mb-4 mars">
 			<form onsubmit="disableButton()" class="p-3" method="POST" action="#" style="background-color: #a4508b;background-image: linear-gradient(326deg, #a4508b 0%, #5f0a87 74%);">
      				<div class="form-group">
         				<label for="inputname">Name</label>
@@ -137,7 +137,7 @@
 				<button type="submit" name="check" id="sent" class="btn btn-dark" style="background-color:black;">Send</button>
 			</form>
 		</div>
-		<div class="col-md-6">
+		<div class="col-md-6 mt-4 mb-4 mars">
 			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15548.576152625568!2d77.5934389!3d13.0264981!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xafffc1080f8f8457!2sSHAPE%20IN%20-%20Women&#39;s%20Fitness%20Gym%20and%20Group%20classes%20RT%20NAGAR!5e0!3m2!1sen!2sin!4v1603537909194!5m2!1sen!2sin" width="100%" height="507" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 		</div>
 	</div>
@@ -151,9 +151,9 @@
 
 <style>
     /*Media Queries*/
-	@media (min-width:320px)  {  /* smartphones, iPhone, portrait 480x320 phones */ }
-	@media (min-width:481px)  {  /* portrait e-readers (Nook/Kindle), smaller tablets @ 600 or @ 640 wide. */ }
- 	@media (min-width:641px)  {  /* portrait tablets, portrait iPad, landscape e-readers, landscape 800x480 or 854x480 phones */ }
+	@media (min-width:320px)  {  .mars{ margin:10px; } /* smartphones, iPhone, portrait 480x320 phones */ }
+	@media (min-width:481px)  {  .mars{ margin:10px; } /* portrait e-readers (Nook/Kindle), smaller tablets @ 600 or @ 640 wide. */ }
+ 	@media (min-width:641px)  {  .mars{ margin:10px; } /* portrait tablets, portrait iPad, landscape e-readers, landscape 800x480 or 854x480 phones */ }
 	@media (min-width:961px)  { .main{object-fit: cover;width: 100%;height: 698px;} /* tablet, landscape iPad, lo-res laptops ands desktops */ }
 	@media (min-width:1025px) { .main{object-fit: cover;width: 100%;height: 698px;} /* big landscape tablets, laptops, and desktops */ }
 	@media (min-width:1281px) { .main{object-fit: cover;width: 100%;height: 698px;}/* hi-res laptops and desktops */ }
