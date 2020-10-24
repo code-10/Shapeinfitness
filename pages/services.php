4306fbe16dbbe9eea015e70234d37b1b15b00314
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-24 18:42:05 +0530

    Update services.php

--- pages/services.php
+++ pages/services.php
@@ -1,4 +1,4 @@
-<?php include_once 'header.php'; ?>
+<?php include_once '../header.php'; ?>
 
 
 
@@ -41,4 +41,4 @@
 
 
 
-<?php include_once 'footer.php'; ?>
+<?php include_once '../footer.php'; ?>
