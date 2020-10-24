bd143d8c5a7add8dcb7bdc51a92dc24640fc0a1c
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-24 10:37:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -47,10 +47,9 @@
 <div class="container m-5"> 
 <a href="#">
     <figure>
-        <div class="date"><span class="card-date-day">20</span><span class="card-date-month">APR</span></div>
         <figcaption>
-            <h4> <span>The arsonist had oddly shaped feet</span></h4>
-            <p>The magic Indian is a mysterious spiritual force, and we're going to Cathedral Rock, and that's the vortex of the heart.</p>
+            <h4> <span>Cardio and Yoga</span></h4>
+            <p>Shape in fitness provides Cardio and Yoga</p>
         </figcaption>
     </figure>
 </a>
