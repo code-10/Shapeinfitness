30eb4f507b204cd2221b5622bf69bb5775cc57f5
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-24 12:16:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
 <!--services-->
 <div id="card" class="m-4">
    <div class="row">
-      <div class="col-md-3 m-2">
+      <div class="col-md-3 mars">
          <a href="#card">
             <figure>
                <figcaption>
@@ -57,7 +57,7 @@
             </figure>
          </a>
       </div>
-      <div class="col-md-3 m-2">
+      <div class="col-md-3 mars">
          <a href="#card">
             <figure>
                <figcaption>
@@ -67,7 +67,7 @@
             </figure>
          </a>
       </div>
-      <div class="col-md-3 m-2">
+      <div class="col-md-3 mars">
          <a href="#card">
             <figure>
                <figcaption>
@@ -77,7 +77,7 @@
             </figure>
          </a>
       </div>
-      <div class="col-md-3 m-2">
+      <div class="col-md-3 mars">
          <a href="#card">
             <figure>
                <figcaption>
@@ -100,9 +100,9 @@
 
 <style>
     /*Media Queries*/
-	@media (min-width:320px)  {  /* smartphones, iPhone, portrait 480x320 phones */ }
-	@media (min-width:481px)  {  /* portrait e-readers (Nook/Kindle), smaller tablets @ 600 or @ 640 wide. */ }
- 	@media (min-width:641px)  {  /* portrait tablets, portrait iPad, landscape e-readers, landscape 800x480 or 854x480 phones */ }
+	@media (min-width:320px)  { .mars{margin:10px;} /* smartphones, iPhone, portrait 480x320 phones */ }
+	@media (min-width:481px)  {  .mars{margin:10px;}/* portrait e-readers (Nook/Kindle), smaller tablets @ 600 or @ 640 wide. */ }
+ 	@media (min-width:641px)  {  .mars{margin:10px;}/* portrait tablets, portrait iPad, landscape e-readers, landscape 800x480 or 854x480 phones */ }
 	@media (min-width:961px)  { .main{object-fit: cover;width: 100%;height: 698px;} /* tablet, landscape iPad, lo-res laptops ands desktops */ }
 	@media (min-width:1025px) {  .main{object-fit: cover;width: 100%;height: 698px;} /* big landscape tablets, laptops, and desktops */ }
 	@media (min-width:1281px) {   .main{object-fit: cover;width: 100%;height: 698px;}/* hi-res laptops and desktops */ }
