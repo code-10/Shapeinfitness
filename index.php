d5c493570d9097b1d84f9a904d7ad8fcc281b080
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-25 15:24:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -80,17 +80,17 @@
 		<? for($j=0;$j<3;$j++) { ?>
 		<div class="col">
 			<figure class="snip1529">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/pr-sample13.jpg" alt="pr-sample13" />
-	<div class="date"><span class="day">28</span><span class="month">Apr</span>
-	</div>
-	<figcaption>
-		<h3>An Abstract Post Heading</h3>
-		<p>Which is worse, that everyone has his price, or that the price is always so low.</p>
-	</figcaption>
-	<div class="hover"><i class="ion-android-open"></i>
-	</div>
-	<a href="#"></a>
-</figure>
+				<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/pr-sample13.jpg" alt="pr-sample13" />
+				<!--<div class="date"><span class="day">28</span><span class="month">Apr</span></div>-->
+				<figcaption>
+					<h3>An Abstract Post Heading</h3>
+					<p>Which is worse, that everyone has his price, or that the price is always so low.</p>
+				</figcaption>
+				<div class="hover">
+					<i class="ion-android-open"></i>
+				</div>
+				<!--<a href="#"></a>-->
+			</figure>
 		</div>
 		<? } ?>
 	</div>
