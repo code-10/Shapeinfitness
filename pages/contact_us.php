b23b6901243754428c29a8195b5303358f35ef31
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-26 20:30:46 +0530

    Update contact_us.php

--- pages/contact_us.php
+++ pages/contact_us.php
@@ -20,7 +20,7 @@
         <a class="nav-link" href="../index.php">Home</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Services</a>
+        <a class="nav-link" href="#services">Services</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="../index.php#gallery">Gallery</a>
