edff085d5df9294926aecd2c3f218c935dbe89dc
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-26 20:17:19 +0530

    Update contact_us.php

--- pages/contact_us.php
+++ pages/contact_us.php
@@ -5,6 +5,57 @@
 	
 ?>
 
+<?php include_once '../header.php'; ?>
+
+
+
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark" style="background-color:#000000;">
+  <a class="navbar-brand" href="../index.php">Shapeinfitness</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="../index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../index.php#gallery">Gallery</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../index.php#contactus">Contact Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../index.php#location">Location</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
+
+
+
+
+
+
+
+<script>
+  
+$(function(){ 
+     var navMain = $(".navbar-collapse");
+
+     navMain.on("click", "a", null, function () {
+         navMain.collapse('hide');
+     });
+ });  
+  
+</script>
+
+
 
 <?php
   
@@ -84,6 +135,7 @@
    </div>
 
 
+<?php include_once '../footer.php'; ?>
           
    <? }
     else
@@ -111,6 +163,3 @@
 
 
 
-
-
-<?php include '../header.php'; ?>
