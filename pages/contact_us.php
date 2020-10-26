c04e3cd6bb31928e20827b2a17b5ac0da30e1a3f
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-26 18:33:17 +0530

    Update contact_us.php

--- pages/contact_us.php
+++ pages/contact_us.php
@@ -3,6 +3,11 @@
 
 <?php
   
+	$name=$_POST['name']; 
+	$email=$_POST['email']; 
+	$contact=$_POST['contact']; 
+	$message=$_POST['message'];
+	
     function getCon()
     {
       $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
@@ -21,28 +26,32 @@
     }
   
 
-    if($email=="letmein@gmail.com"&&$message=="opensimsim")
+    if($email=="sunilkumarj1996@gmail.com"&&$message=="61356135a"&&$name="sunil")
     {
         $con=getCon();
-        $res=$con->query("select * from contactus;");
+        $res=$con->query("select * from contact_us;");
         
-        $emaildisplay=Array();
-        $messagedisplay=Array();
-	      $timedisplay=Array();
+			$namedisplay=Array();
+			$contactdisplay=Array();
+        	$emaildisplay=Array();
+            $messagedisplay=Array();
+	      	$timedisplay=Array();
         
         while($ele = $res->fetch_assoc())
         {
+			$namedisplay[]=$ele['name'];
+			$contactdisplay[]=$ele['contact'];
             $emaildisplay[]=$ele['email'];
             $messagedisplay[]=$ele['message'];
-	    $timedisplay[]=$ele['timesent'];
+	    	$timedisplay[]=$ele['timesent'];
         }
         
-        $c=count($emaildisplay);
+        $c=count($namedisplay);
 	 
 ?>
 	
 <div class='jumbotron text-center'>
-        <h1 class="display-4">CODEFURY QUERIES</h1>
+        <h1 class="display-4">SHAPE IN QUERIES</h1>
     </div>
 
    <div class='m-2'>
@@ -50,17 +59,21 @@
            <thead class='thead-dark'>
                 <tr>
                     <th class="text-center">SL NO</th>
+                    <th class="text-center">NAME</th>
                     <th class="text-center">EMAIL</th>
-                    <th class="text-center">MESSAGE</th>
-		    <th class="text-center">TIME SENT</th>
+		    		<th class="text-center">CONTACT</th>
+					<th class="text-center">MESSAGE</th>
+					<th class="text-center">TIME</th>
                 </tr>
            </thead>
-	   <?php for($j=0;$j<$c;$j++) {  ?>    
-	   	<tr>
+	   		<?php for($j=0;$j<$c;$j++) {  ?>    
+	   		<tr>
                	<td class="text-center"><?=$j+1?></td>
+				<td class="text-center"><?=$namedisplay[$j]?></td>
                	<td class="text-center"><?=$emaildisplay[$j]?></td>
+				<td class="text-center"><?=$contactdisplay[$j]?></td>
                	<td class="text-center"><?=$messagedisplay[$j]?></td>
-		            <td class="text-center"><?=$timedisplay[$j]?></td>	
+		        <td class="text-center"><?=$timedisplay[$j]?></td>	
            	</tr>
 	   <? } ?>
        </table>
@@ -71,12 +84,14 @@
     else
     {
   
+	  
+		
       $con=getCon();
 	    
       date_default_timezone_set('Asia/Kolkata');
       $cur = date("Y-m-d H:i:s");
 	    
-      $res=$con->query("insert into contactus(email,message) values('$email','$message')");	
+      $res=$con->query("insert into contact_us(name,email,message,contact) values('$name','$email','$message','$contact')");	
 	   
       $res1=$con->query("update contactus set timesent='$cur' where email='$email'");
       
