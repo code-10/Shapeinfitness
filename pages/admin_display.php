e55036f7a2e62b1b9022b0fa68bec93f7bab2c5b
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-27 13:57:13 +0530

    Update admin_display.php

--- pages/admin_display.php
+++ pages/admin_display.php
@@ -155,12 +155,12 @@ $con=getCon();
   
 <style>
     /*Media Queries*/
-	@media (min-width:320px)  {    /* smartphones, iPhone, portrait 480x320 phones */ }
-	@media (min-width:481px)  {    /* portrait e-readers (Nook/Kindle), smaller tablets @ 600 or @ 640 wide. */ }
- 	@media (min-width:641px)  {    /* portrait tablets, portrait iPad, landscape e-readers, landscape 800x480 or 854x480 phones */ }
-	@media (min-width:961px)  {  .fonty{} /* tablet, landscape iPad, lo-res laptops ands desktops */ }
-	@media (min-width:1025px) {  .fonty{} /* big landscape tablets, laptops, and desktops */ }
-	@media (min-width:1281px) {  .fonty{} /* hi-res laptops and desktops */ }
+	@media (min-width:320px)  {  .fonty{font-size:16px;}  /* smartphones, iPhone, portrait 480x320 phones */ }
+	@media (min-width:481px)  {  .fonty{font-size:16px;} /* portrait e-readers (Nook/Kindle), smaller tablets @ 600 or @ 640 wide. */ }
+ 	@media (min-width:641px)  {  .fonty{font-size:16px;}  /* portrait tablets, portrait iPad, landscape e-readers, landscape 800x480 or 854x480 phones */ }
+	@media (min-width:961px)  {  .fonty{font-size:20px;} /* tablet, landscape iPad, lo-res laptops ands desktops */ }
+	@media (min-width:1025px) {  .fonty{font-size:20px;} /* big landscape tablets, laptops, and desktops */ }
+	@media (min-width:1281px) {  .fonty{font-size:20px;} /* hi-res laptops and desktops */ }
 	
 </style>
   
