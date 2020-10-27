dae80f114ad87a35acb328bed3eeff943c2ac4e0
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-27 10:26:42 +0530

    Update contact_us.php

--- pages/contact_us.php
+++ pages/contact_us.php
@@ -44,7 +44,8 @@
       $con=getCon();
 	    
       date_default_timezone_set('Asia/Kolkata');
-      $cur = date("Y-m-d H:i:s");
+      //$cur = date("Y-m-d H:i:s");
+      $cur = date("F j, Y, g:i a T");	
 	    
       $res=$con->query("insert into contact_us(name,email,message,contact) values('$name','$email','$message','$contact')");	
 	   
