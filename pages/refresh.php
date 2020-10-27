79f251e34d5c5023c293629cfd2a7df9800da947
Manoj Kumar <manojkumar333619@gmail.com>
2020-10-27 13:27:33 +0530

    Create refresh.php

---
+++ pages/refresh.php
@@ -0,0 +1,27 @@
+<?php
+
+
+        function getCon()
+    {
+      $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+      $server = $url["host"];
+      $username = $url["user"];
+      $password = $url["pass"];
+      $db = substr($url["path"], 1);
+
+      $con = new mysqli($server, $username, $password, $db);
+
+      if($con->connect_error)
+        die($con->connect_error);
+
+      return $con;
+
+    }
+    
+    $con->query("delete from contact_us where name="" ");
+    
+    header("Location:admin_display.php");
+    die();
+
+
+?>
