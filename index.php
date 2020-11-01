b689f68e1159dea69b68ea171aabaa3c3820815d
Manoj Kumar <manojkumar333619@gmail.com>
2020-11-01 21:32:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -156,37 +156,37 @@
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="img-fluid" src="assets/gallery/gallery_1.jpg" alt="First slide">
+      <img class="img-fluid size" src="assets/gallery/gallery_1.jpg" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="img-fluid" src="assets/gallery/gallery_2.jpg" alt="Second slide">
+      <img class="img-fluid size" src="assets/gallery/gallery_2.jpg" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="img-fluid" src="assets/gallery/gallery_3.jpg" alt="Third slide">
+      <img class="img-fluid size" src="assets/gallery/gallery_3.jpg" alt="Third slide">
     </div>
     <div class="carousel-item">
-      <img class="img-fluid" src="assets/gallery/gallery_4.jpg" alt="Fourth slide">
+      <img class="img-fluid size" src="assets/gallery/gallery_4.jpg" alt="Fourth slide">
     </div>
     <div class="carousel-item">
-      <img class="img-fluid" src="assets/gallery/gallery_5.jpg" alt="fifth slide">
+      <img class="img-fluid size" src="assets/gallery/gallery_5.jpg" alt="fifth slide">
     </div>
     <div class="carousel-item">
-      <img class="img-fluid" src="assets/gallery/gallery_6.jpg" alt="sixth slide">
+      <img class="img-fluid size" src="assets/gallery/gallery_6.jpg" alt="sixth slide">
     </div>
     <div class="carousel-item">
-      <img class="img-fluid" src="assets/gallery/gallery_7.jpg" alt="seventh slide">
+      <img class="img-fluid size" src="assets/gallery/gallery_7.jpg" alt="seventh slide">
     </div>
     <div class="carousel-item">
-      <img class="img-fluid" src="assets/gallery/gallery_8.jpg" alt="eight slide">
+      <img class="img-fluid size" src="assets/gallery/gallery_8.jpg" alt="eight slide">
     </div>
     <div class="carousel-item">
-      <img class="img-fluid" src="assets/gallery/gallery_9.jpg" alt="ninth slide">
+      <img class="img-fluid size" src="assets/gallery/gallery_9.jpg" alt="ninth slide">
     </div>
     <div class="carousel-item">
-      <img class="img-fluid" src="assets/gallery/gallery_10.jpg" alt="tenth slide">
+      <img class="img-fluid size" src="assets/gallery/gallery_10.jpg" alt="tenth slide">
     </div>
     <div class="carousel-item">
-      <img class="img-fluid" src="assets/gallery/gallery_11.jpg" alt="eleven slide">
+      <img class="img-fluid size" src="assets/gallery/gallery_11.jpg" alt="eleven slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -317,9 +317,9 @@ $(function(){
 
 <style>
     /*Media Queries*/
-	@media (min-width:320px)  {  .mars{ margin:10px; } .size{ object-fit: cover;width: 100%;height: 624px; }  /* smartphones, iPhone, portrait 480x320 phones */ }
-	@media (min-width:481px)  {  .mars{ margin:10px; } .size{ object-fit: cover;width: 100%;height: 624px; }  /* portrait e-readers (Nook/Kindle), smaller tablets @ 600 or @ 640 wide. */ }
- 	@media (min-width:641px)  {  .mars{ margin:10px; } .size{ object-fit: cover;width: 100%;height: 624px; }  /* portrait tablets, portrait iPad, landscape e-readers, landscape 800x480 or 854x480 phones */ }
+	@media (min-width:320px)  {  .mars{ margin:10px; }   /* smartphones, iPhone, portrait 480x320 phones */ }
+	@media (min-width:481px)  {  .mars{ margin:10px; }   /* portrait e-readers (Nook/Kindle), smaller tablets @ 600 or @ 640 wide. */ }
+ 	@media (min-width:641px)  {  .mars{ margin:10px; }  /* portrait tablets, portrait iPad, landscape e-readers, landscape 800x480 or 854x480 phones */ }
 	@media (min-width:961px)  { .main{object-fit: cover;width: 100%;height: 698px;} .size{ object-fit: cover;width: 100%;height: 624px; }  /* tablet, landscape iPad, lo-res laptops ands desktops */ }
 	@media (min-width:1025px) { .main{object-fit: cover;width: 100%;height: 698px;} .size{ object-fit: cover;width: 100%;height: 624px; }  /* big landscape tablets, laptops, and desktops */ }
 	@media (min-width:1281px) { .main{object-fit: cover;width: 100%;height: 698px;} .size{ object-fit: cover;width: 100%;height: 624px; }  /* hi-res laptops and desktops */ }
