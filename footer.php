f6dcef626c7a30061ed3258425122022e3c873fa
Manoj Kumar <manojkumar333619@gmail.com>
2020-11-02 21:49:54 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -67,21 +67,7 @@ background-image: linear-gradient(315deg, #42378f 0%, #f53844 74%);color:white;"
     
   </div>
   
- <style>
-   .footer-heart{text-align:center;font-family: open sans, sans-serif;}
-
-.footer-heart a{border-bottom: 1px solid #453886;color: #ffffff;padding-bottom: .25em; text-decoration: none;}
-
-.footer-heart a:hover{color:#ffffff; background-image: url("data:image/svg+xml;charset=utf8,%3Csvg id='squiggle-link' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:ev='http://www.w3.org/2001/xml-events' viewBox='0 0 20 4'%3E%3Cstyle type='text/css'%3E.squiggle{animation:shift .3s linear infinite;}@keyframes shift {from {transform:translateX(0);}to {transform:translateX(-20px);}}%3C/style%3E%3Cpath fill='none' stroke='%23453886' stroke-width='2' class='squiggle' d='M0,3.5 c 5,0,5,-3,10,-3 s 5,3,10,3 c 5,0,5,-3,10,-3 s 5,3,10,3'/%3E%3C/svg%3E");
-  background-position: bottom;
-  background-repeat: repeat-x;
-  background-size: 20%;
-  border-bottom: 0;
-  padding-bottom: .3em;
-  text-decoration: none;}
-
-.emoji{vertical-align: middle;}
-  </style>
+ 
   
   <!-- Copyright -->
 
