6871a196955f53c0e1dadc2fcbd9ba7ade6e50f4
Manoj Kumar <manojkumar333619@gmail.com>
2020-11-02 21:40:09 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -58,7 +58,32 @@ background-image: linear-gradient(315deg, #42378f 0%, #f53844 74%);color:white;"
   <!-- Copyright -->
   <div class="footer-copyright text-center py-3">©2020 <a style="color:white" href="www.shapeinfitness.in">SHAPE IN</a>, All rights reserved.
   <!--<a href="#" style="color:white;">SHAPE IN</a>-->
+  
+  <!--made with love--> 
+    <p class="footer-heart">
+  Made with <g-emoji class="g-emoji" alias="heart" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png">
+<img class="emoji" alt="heart" height="20" width="20" src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png"></g-emoji> by <a href="https://armin.id">ANT GROUND</a>
+</p>
+  <!--made with love end-->  
+    
   </div>
+  
+ <style>
+   .footer-heart{text-align:center;font-family: open sans, sans-serif;}
+
+.footer-heart a{border-bottom: 1px solid #453886;color: #453886;padding-bottom: .25em; text-decoration: none;}
+
+.footer-heart a:hover{color:#2196f3; background-image: url("data:image/svg+xml;charset=utf8,%3Csvg id='squiggle-link' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:ev='http://www.w3.org/2001/xml-events' viewBox='0 0 20 4'%3E%3Cstyle type='text/css'%3E.squiggle{animation:shift .3s linear infinite;}@keyframes shift {from {transform:translateX(0);}to {transform:translateX(-20px);}}%3C/style%3E%3Cpath fill='none' stroke='%23453886' stroke-width='2' class='squiggle' d='M0,3.5 c 5,0,5,-3,10,-3 s 5,3,10,3 c 5,0,5,-3,10,-3 s 5,3,10,3'/%3E%3C/svg%3E");
+  background-position: bottom;
+  background-repeat: repeat-x;
+  background-size: 20%;
+  border-bottom: 0;
+  padding-bottom: .3em;
+  text-decoration: none;}
+
+.emoji{vertical-align: middle;}
+  </style>
+  
   <!-- Copyright -->
 
 </footer>
