130d03da9679b307fda10cc328606b7faa3158c6
Manoj Kumar <manojkumar333619@gmail.com>
2020-11-02 22:28:52 +0530

    Update delete_query.php

--- pages/delete_query.php
+++ pages/delete_query.php
@@ -1,1 +1,30 @@
-<?php?>
+<?php
+
+ function getCon()
+    {
+      $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+      $server = $url["host"];
+      $username = $url["user"];
+      $password = $url["pass"];
+      $db = substr($url["path"], 1);
+
+      $con = new mysqli($server, $username, $password, $db);
+
+      if($con->connect_error)
+        die($con->connect_error);
+
+      return $con;
+
+    }
+    
+    $contact_id=$_POST['contact_id'];
+    $con=getCon();
+
+    $con->query("delete from contact_us where contact_id='$contact_id'");
+    
+    header("Location:admin_display.php");
+    die();
+
+
+
+?>
