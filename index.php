d2d01ec21b5e1deba6084d507b1ad6ef58d60f4a
Manoj Kumar <manojkumar333619@gmail.com>
2020-11-03 13:37:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -47,13 +47,19 @@
 
 
 <!--Main-->
-<div class="hero-image">
+
+<!--<div class="hero-image">
   <div class="hero-text">
-    <!--<h1 class="animate__animated animate__fadeIn" style="color:white;font-size:64px;animation-duration: 2s;">SHAPE IN</h1>
+    <h1 class="animate__animated animate__fadeIn" style="color:white;font-size:64px;animation-duration: 2s;">SHAPE IN</h1>
     <p>Women's Fitness Gym</p>
-    <a href="#learnmore"><button class="btn draw-border" style="padding:1em;">Learn More</button></a>-->
+    <a href="#learnmore"><button class="btn draw-border" style="padding:1em;">Learn More</button></a>
   </div>
-</div>
+</div>-->
+
+
+<img src="assets/gallery/gallery_9.webp" class="img-fluid" alt="SHAPE IN">
+
+
 <!--main end-->
 
 
