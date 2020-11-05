e847a12ac645dce5071847f8b892433e14462717
Manoj Kumar <manojkumar333619@gmail.com>
2020-11-05 10:11:37 +0530

    Update header.php

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="SHAPE IN - RT NAGAR" content="SHAPE IN - Women's Fitness Gym and Group Classes RT NAGAR">
     <link rel="shortcut icon" type="image/x-icon" href="assets/favicon.ico">
     <title>SHAPE IN - Women's Fitness Gym and Group Classes RT NAGAR</title>
   
