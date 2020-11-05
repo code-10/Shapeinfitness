6635a8a9867a535fc31c2446ac164d62baddf03a
Manoj Kumar <manojkumar333619@gmail.com>
2020-11-05 12:12:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,60 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>SHAPE IN - Women's Fitness Gym and Group Classes RT NAGAR</title>
+  
+
+
+    <!--seo-->
+    <meta name="description" content="SHAPE IN - best Women's gym near me - Women's Fitness Gym and Group Classes RT NAGAR"/>
+  <link rel="canonical" href="https://www.shapeinfitness.in/"/>
+  <meta property="og:title" content="SHAPE IN - BEST GYM in R T NAGAR "/>
+  <meta property="og:description" content="SHAPE IN PROVIDES WEIGHT LOSS, YOGA, ZUMBA, PILATES, TONING. "/>
+  <meta property="og:image" content="assets/logo.webp"/>
+  <meta property="og:image:width" content="998"/>
+  <meta property="og:image:height" content="223"/>
+  <meta property="og:url" content="https://www.shapeinfitness.in/"/>
+  <meta property="og:site_name" content="Shape in Fitness "/>
+  <meta property="og:type" content="website"/>
+  <meta name="google-site-verification" content=""/>
+  <meta name="keywords" content="BEST GYM NEAR ME, SHAPE IN, WOMEN'S GYM, R T NAGAR, FITNESS "/>
+    <!--seo end-->
+
+
+
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/favicon.ico">
+    
+    <!--bootstrap link-->
+    <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    
+    <!--font awesome-->  
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  
+    <!--bootstrap js-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    
+     <!-- Compiled and minified CSS -->
+    <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">-->
+
+    <!-- Compiled and minified JavaScript -->
+    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>-->
+</head>
+
+
+
+
+
+
 <?php 
-	include 'header.php';
 	session_start();
 ?>
 
