b8b11b3e035f12815f19f8b32a4e73165540bc55
Manoj Kumar <manojkumar333619@gmail.com>
2020-11-05 09:51:29 +0530

    Update header.php

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/x-icon" href="assets/favicon.ico">
-    <title>Shapeinfitness</title>
+    <title>SHAPE IN - Women's Fitness Gym and Group Classes RT NAGAR</title>
   
     <!--bootstrap link-->
     <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
