83a110d9b8363130371187123cbc8fc3cc6411b5
Manoj Kumar <manojkumar333619@gmail.com>
2020-11-05 09:40:24 +0530

    Update header.php

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="image/x-icon" sizes="192x192" href="assets/logo.webp">
+    <link rel="shortcut icon" type="image/x-icon" sizes="16x16" href="assets/logo.webp">
     <title>Shapeinfitness</title>
   
     <!--bootstrap link-->
